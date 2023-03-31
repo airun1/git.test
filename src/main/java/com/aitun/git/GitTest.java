7389d47ea74d62b5be84e4cfdb2fe7d162cb2cee
airun <yd15875022088@163.com>
2023-03-31 09:00:50 +0800

    First commit

---
+++ src/main/java/com/aitun/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.aitun.git;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ *
+ * @Author 蔡远东
+ * @Create 2023/3/31 8:56
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
