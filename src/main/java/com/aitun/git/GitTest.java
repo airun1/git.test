21b757e0a41c48d35c25f85bb06fa1b3e036f021
airun <yd15875022088@163.com>
2023-03-31 09:04:11 +0800

    Third commit

--- src/main/java/com/aitun/git/GitTest.java
+++ src/main/java/com/aitun/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
         System.out.println("Hello Git2");
+        System.out.println("Hello Git2");
 
     }
 }
